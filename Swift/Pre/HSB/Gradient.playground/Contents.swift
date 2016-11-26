757a7169e13ab93ab4b58797b9e56166fa653cbf
Apollo Zhu <public-apollonian@outlook.com>
2016-11-26 18:08:10 -0500

    Shorten Code

--- Swift/Pre/HSB/Gradient.playground/Contents.swift
+++ Swift/Pre/HSB/Gradient.playground/Contents.swift
@@ -15,14 +15,14 @@ class HueGradientLayer: CAGradientLayer {
         setup()
     }
     private func setup() {
-        var controls = [Double]()
+        var controls = [CGFloat]()
 //        controls = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-        controls.append(contentsOf: stride(from: 0, through: 1, by: 0.1))
+        controls = Array(stride(from: 0, through: 1, by: 0.1))
         locations = controls as [NSNumber]?
 //        colors = controls.map({ (hue) -> CGColor in
 //            UIColor(hue: CGFloat(hue), saturation: 1, brightness: 1, alpha: 1).cgColor
 //        })
-        colors = controls.map { UIColor(hue: CGFloat($0), saturation: 1, brightness: 1, alpha: 1).cgColor }
+        colors = controls.map { UIColor(hue: $0, saturation: 1, brightness: 1, alpha: 1).cgColor }
         startPoint = CGPoint(x: 0, y: 0.5)
         endPoint = CGPoint(x: 1, y: 0.5)
     }
@@ -34,9 +34,9 @@ class HueGradientView: UIView {
     }
 }
 
-let view = HueGradientView(frame: CGRect(x:0,y:0,width:100,height:100))
+//let view = HueGradientView(frame: CGRect(x:0,y:0,width:100,height:100))
 
-//let view = HueGradientView(frame: CGRect(x:0,y:0,width:685,height:30))
+let view = HueGradientView(frame: CGRect(x:0,y:0,width:685,height:30))
 
 
 
