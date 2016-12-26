9de9d19837092089d490649d8b38f37e038a3d79
Apollo Zhu <public-apollonian@outlook.com>
2016-12-26 14:06:46 -0500

    CAGradientLayer
    Cover original [ミクツリー by
    BEEK@三日目西め02b](http://www.pixiv.net/member_illust.php?mode=medium&illust
    _id=40471161)

--- Swift/av7711652/CAGradientLayer.playground/Contents.swift
+++ Swift/av7711652/CAGradientLayer.playground/Contents.swift
@@ -1,6 +1,5 @@
 import UIKit
 
-@IBDesignable
 class HueGradientLayer: CAGradientLayer {
     override init() {
         super.init()
@@ -14,29 +13,35 @@ class HueGradientLayer: CAGradientLayer {
         super.init(coder: aDecoder)
         setup()
     }
+
     private func setup() {
+
         var controls = [CGFloat]()
-//        controls = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-        controls = Array(stride(from: 0, through: 1, by: 0.1))
+        // controls = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+        // No longer supported: for (var i=0.0;i<=1;i+=0.1) { controls.append(i) }
+        // Alternative: for i in stride(from: 0, through: 1, by: 0.1) { controls.append(i) }
+        controls = Array(stride(from: 0, through: 1, by: 0.01))
+
         locations = controls as [NSNumber]?
-//        colors = controls.map({ (hue) -> CGColor in
-//            UIColor(hue: CGFloat(hue), saturation: 1, brightness: 1, alpha: 1).cgColor
-//        })
+
+        /*
+         colors = []
+         for stop in controls {
+            colors?.append(UIColor(hue: stop, saturation: 1, brightness: 1, alpha: 1).cgColor)
+         }
+         */
         colors = controls.map { UIColor(hue: $0, saturation: 1, brightness: 1, alpha: 1).cgColor }
+
         startPoint = CGPoint(x: 0, y: 0.5)
         endPoint = CGPoint(x: 1, y: 0.5)
     }
 }
 
 class HueGradientView: UIView {
-    override open class var layerClass: AnyClass {
+    override open class var layerClass: Swift.AnyClass {
         return HueGradientLayer.self
     }
 }
 
-//let view = HueGradientView(frame: CGRect(x:0,y:0,width:100,height:100))
-
-let view = HueGradientView(frame: CGRect(x:0,y:0,width:685,height:30))
-
-
+let view = HueGradientView(frame: CGRect(x: 0, y: 0, width: 685, height: 30))
 
